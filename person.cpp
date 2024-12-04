76f618f6debf965ba8e7efee9b0879b7ab931722
MaxTham <wingfei5778@gmail.com>
2024-12-04 21:17:42 +0800

    execution test

--- person.cpp
+++ person.cpp
@@ -1,42 +1,46 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-class Person{
-    private:
-        int age;
-        char name[25],address[25],email[25],phoneNo[25];
+class Person
+{
+private:
+    int age;
+    char name[25], address[25], email[25], phoneNo[25];
 
-    public:
-        void getPersonData(){
-            cout<<"Enter name: ";
-            cin>>name; 
+public:
+    void getPersonData()
+    {
+        cout << "Enter name: ";
+        cin >> name;
 
-            cout<<"Enter age: ";
-            cin>>age;
+        cout << "Enter age: ";
+        cin >> age;
 
-            cout<<"Enter phone number: ";
-            cin>>phoneNo;
+        cout << "Enter phone number: ";
+        cin >> phoneNo;
 
-            cout<<"Enter address: ";
-            cin>>address;
+        cout << "Enter address: ";
+        cin >> address;
 
-            cout<<"Enter email: ";
-            cin>>email;
-        }
-        
-        void showPersonData(){
-            cout<<"Name: "<<name<<endl;
-            cout<<"Age: "<<age<<endl;
-            cout<<"Phone number: "<<phoneNo<<endl;
-            cout<<"Address: "<<address<<endl;
-            cout<<"Email: "<<email<<endl;
-        }
+        cout << "Enter email: ";
+        cin >> email;
+    }
+
+    void showPersonData()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+        cout << "Phone number: " << phoneNo << endl;
+        cout << "Address: " << address << endl;
+        cout << "Email: " << email << endl;
+    }
 };
 
-int main(){
-    Person p1;
+// For testing purposes
+ int main(){
+     Person p1;
 
     p1.getPersonData();
     p1.showPersonData();
