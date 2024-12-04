7650710adada4401c5fb7969cc9475e7f6ac87fc
MaxTham <wingfei5778@gmail.com>
2024-12-04 20:39:34 +0800

    Person cpp

--- person.cpp
+++ person.cpp
@@ -8,5 +8,38 @@ class Person{
         int age;
         char name[25],address[25],email[25],phoneNo[25];
 
-    test
+    public:
+        void getPersonData(){
+            cout<<"Enter name: ";
+            cin>>name; 
+
+            cout<<"Enter age: ";
+            cin>>age;
+
+            cout<<"Enter phone number: ";
+            cin>>phoneNo;
+
+            cout<<"Enter address: ";
+            cin>>address;
+
+            cout<<"Enter email: ";
+            cin>>email;
+        }
+        
+        void showPersonData(){
+            cout<<"Name: "<<name<<endl;
+            cout<<"Age: "<<age<<endl;
+            cout<<"Phone number: "<<phoneNo<<endl;
+            cout<<"Address: "<<address<<endl;
+            cout<<"Email: "<<email<<endl;
+        }
 };
+
+int main(){
+    Person p1;
+
+    p1.getPersonData();
+    p1.showPersonData();
+
+    return 0;
+}
