5d38c5685200f4cfb256c5cedeb6b9a01f8cdae8
MaxTham <wingfei5778@gmail.com>
2024-12-10 15:36:53 +0800

    patient.cpp

--- patient.cpp
+++ patient.cpp
@@ -1,9 +1,7 @@
 #include "person.cpp"
 #include "disease.cpp"
-#include <iostream>
+#include <fstream>
 #include <cstring>
-
-using namespace std;
 
 class patient : public Person
 {
@@ -12,83 +10,203 @@ private:
     Disease disease;
     string appointDate;
     char patientFile[20] = "patientData.dat";
-    char patientTempFile[20] = "patientdata2.dat";
+    char patientTempFile[20] = "patientData2.dat";
 
 public:
     void getPatientInformation()
     {
-        cout << "Enter person's data" << endl;
+        // getting patient information using person class
+        cout << "Enter the data for the person";
         getPersonData();
 
+        // getting disease, appointDate
         cout << "Disease details" << endl;
+        // cin.get();
         disease.getDiseaseDetail();
 
-        cout << "Appoint date" << endl;
+        cout << "Appoint date: ";
         cin >> appointDate;
     }
 
+    // add patient to the file
     void addPatient()
     {
-        cout << "Patient ID : ";
+        // getting patientId
+        cout << "Patient Id: ";
         cin >> patientId;
 
         getPatientInformation();
         ofstream patientDatabase(patientFile, ios::binary | ios::out | ios::app);
-
         patientDatabase.write((char *)this, sizeof(patient));
     }
 
-    void showPatientDetails()
+    // show patient data
+    void showPatientData()
     {
-        cout << "---Patient Data---" << endl;
-        cout << "Patient Id : " << patientId << endl;
+        cout << "\n\n\t\t---- PATIENT DATA ----" << endl;
+        cout << "Patient Id: " << patientId << endl;
         showPersonData();
-        disease.showDiseaseList();
-        cout << "Appointment date : " << appointDate << endl;
+        disease.showDiseaseDetail();
+        cout << "Appointment date: " << appointDate << endl;
     }
 
+    // showing patient data from file
     void showPatientDataFromFile()
     {
         ifstream readdatafromdatabase(patientFile, ios::binary | ios::in);
-
-        while (!readdatafromdatabase.read((char *)this, sizeof(patient)))
+        while (!readdatafromdatabase.eof())
+        {
             if (readdatafromdatabase.read((char *)this, sizeof(patient)))
                 showPatientData();
+        }
     }
 
+    // deleting patient details from file
     void dischargePatient()
     {
         int n, flag = 0;
-
-        cout << "\nEnter the ID of the patient you want to delete" << endl;
+        cout << "\nEnter the Id of patient you want to delete" << endl;
         cin >> n;
 
         ifstream ifpatient;
-        ifpatient.open(patientFile, ios::in | ios::binary)
-
+        ifpatient.open(patientFile, ios::in | ios::binary);
         ofstream ofpatient;
-        ofpatient.open(patientTempFile,ios::out|ios::binary);
+        ofpatient.open(patientTempFile, ios::out | ios::binary);
 
-        while(!ifpatient.eof()){
-            ifpatient.read((char*)this,sizeof(patient));
-            if(ifpatient){
-                if(n==patientId){
+        while (!ifpatient.eof())
+        {
+            ifpatient.read((char *)this, sizeof(patient));
+            if (ifpatient)
+            {
+
+                // comparing the patient Id with that
+                // of the entered patient Id
+                if (n == patientId)
+                {
                     flag = 1;
-                }e;se{
-                    ofpatient.write((char*)this,sizeof(patient));
+                }
+                else
+                {
+                    ofpatient.write((char *)this, sizeof(patient));
                 }
             }
         }
-
         ifpatient.close();
         ofpatient.close();
         remove(patientFile);
-        rename(patientTempFile,patientFile);
+        rename(patientTempFile, patientFile);
+        showPatientDataFromFile();
 
-        if(flag==1){
-            cout<<"\nRecord successfully deleted \n";
-        }else{
-            cout<<"\nRecord not found\n";
-        }
+        if (flag == 1)
+            cout << "\nRecord successfully deleted \n";
+        else
+            cout << "\nRecord not found \n";
     }
-}
+
+    void editPatient()
+    {
+        int n, pos, flag = 0;
+        cout << "\nEnter the Id of patient you want to edit" << endl;
+        cin >> n;
+
+        fstream fpatient;
+        fpatient.open(patientFile, ios::binary | ios::in | ios::out);
+
+        while (!fpatient.eof())
+        {
+            // storing the position of
+            // current file pointeri.e. at
+            // the end of previously read record
+            pos = fpatient.tellg();
+
+            fpatient.read((char *)this, sizeof(patient));
+            if (fpatient)
+            {
+
+                // comparing the patient Id with that
+                // of the entered patient Id
+                if (n == patientId)
+                {
+                    flag = 1;
+
+                    // setting the new (modified )
+                    // data of the object or new record
+                    getPatientInformation();
+
+                    // placing the put(writing) pointer
+                    // at the starting of the  record
+                    fpatient.seekp(pos);
+
+                    // writing the object to the file
+                    fpatient.write((char *)this, sizeof(patient));
+
+                    // display the data
+                    showPatientData();
+                    break;
+                }
+            }
+        }
+        fpatient.close();
+
+        if (flag == 1)
+            cout << "\nRecord successfully modified \n";
+        else
+            cout << "\nRecord not found \n";
+    }
+
+    void doPatientRelatedWork()
+    {
+        char flag;
+        int choice;
+        do
+        {
+            cout << "Enter option 1 to add patient " << endl;
+            cout << "Enter option 2 to show all data " << endl;
+            cout << "Enter option 3 to discharge patient " << endl;
+            cout << "Enter option 4 to edit patient's data " << endl;
+            cout << "Enter option 5 to exit program " << endl;
+            cin >> choice;
+
+            switch (choice)
+            {
+            case 1:
+                do
+                {
+                    addPatient();
+                    cout << "Add another patient? Enter '1' for yes, any other key to stop: ";
+                    cin >> flag;
+                } while (flag == '1'); // Ensure flag is checked correctly for character input
+                break;
+            case 2:
+                showPatientDataFromFile();
+                break;
+            case 3:
+                dischargePatient();
+                break;
+            case 4:
+                editPatient();
+                break;
+            case 5:
+                cout << "Exiting the program. Goodbye!" << endl;
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+            }
+
+        } while (choice != 5); // Exit loop when the user selects option 5
+    }
+};
+
+// for testing purpose
+// int main()
+// {
+//     patient p;
+//     p.addPatient();
+//     p.showPatientDataFromFile();
+//     p.dischargePatient();
+//     p.editPatient();
+
+//     p.doPatientRelatedWork();
+
+//     return 0;
+// }
